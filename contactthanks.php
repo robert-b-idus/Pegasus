4b90166b6f3a33324c4b08845c797e8a0a5067c2
Robert Björkman <RobertBjorkman@Idus01.local>
2015-11-13 11:22:39 +0100

    Checkar in källkoden

---
+++ contactthanks.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"><html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="description" content="Premium HTML Template by wwww.creativestable.com" />
+    <meta name="keywords" content="" />
+    <title>Message Sent | Paragon Stables</title>
+    <link rel="icon" type="image/ico" href="favicon.ico" /> 
+    
+    <link href="css/reset.css" media="all" rel="stylesheet" type="text/css" />
+    <link href="css/style.css" media="all" rel="stylesheet" type="text/css" /> 
+    <!--[if lt IE 8]>
+    <link rel="stylesheet" type="text/css" href="css/ie.css" />
+    <script type="text/javascript" src="js/dd_belatedpng.js"></script>
+    <script> DD_belatedPNG.fix('.pngfix'); </script>
+    <![endif]--> 
+</head>
+
+<body>
+<div class="top-bar"></div><!--End top-bar -->
+<div id="container">
+
+    <div id="header">
+        <h1 class="site-logo"><a href="index.html" class="pngfix">Paragon Stables</a></h1>
+        <div id="navigation">
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="sales.html">Sales</a></li>
+                <li><a href="news.html">News</a></li>
+                <li><a href="contact.html">Contact</a></li>
+            </ul>
+            <div class="search">
+                <form action="#" method="post">
+                    <p><input type="text" name="search" class="form-text" />
+                    <input type="submit" value="Search" class="form-submit" /></p>
+                </form> 
+            </div><!--End search  -->
+        </div><!--End navigation -->
+    </div><!--End header -->
+    
+    <div id="primary">
+            <h3>&#187; Your Message Has Been Sent</h3>
+            <h1 class="page-title">Success!</h1>
+            <p>Thanks for your message! We will get back to you as soon as possible!</p>
+    </div><!--End primary -->
+    
+    <div class="clear-block"></div>
+    
+    <div id="bottom-container">
+    <div id="secondary">
+        <div class="inside">
+            <h3>&#187; Current Residents</h3>
+            <div class="column">
+                <div class="item"><img src="files/residents/1.jpg" alt="Resident 1" /><h4><a href="#">UVM Flash</a></h4><p>16.1hh Morgan Stallion</p></div><!--End item-->
+                <div class="item"><img src="files/residents/2.jpg" alt="Resident 2" /><h4><a href="#">Anacacho Breeze</a></h4><p>16.3hh Saddlebred Mare</p></div><!--End item--> 
+                <div class="item"><img src="files/residents/3.jpg" alt="Resident 3" /><h4><a href="#">SIH Surprise Bess</a></h4><p>15.3hh Morgan Mare</p></div><!--End item-->  
+                <div class="item"><img src="files/residents/4.jpg" alt="Resident 4" /><h4><a href="#">Navaduur</a></h4><p>17.2hh Hanoverian Stallion</p></div><!--End item--> 
+            </div><!--End column-->
+            
+            <div class="column">
+                <div class="item"><img src="files/residents/5.jpg" alt="Resident 5" /><h4><a href="#">Sinclaire</a></h4><p>16.1hh Dutch Warmblood Mare</p></div><!--End item--> 
+                <div class="item"><img src="files/residents/6.jpg" alt="Resident 6" /><h4><a href="#">Antiquity</a></h4><p>16.1hh AWS Mare</p></div><!--End item-->       
+                <div class="item"><img src="files/residents/7.jpg" alt="Resident 7" /><h4><a href="#">Kongur fra Stora Hof</a></h4><p>14.3hh Icelandic Stallion</p></div><!--End item-->   
+                <div class="item"><img src="files/residents/8.jpg" alt="Resident 8" /><h4><a href="#">Skips KID</a></h4><p>15.2hh AQHA Stallion</p></div><!--End item-->      
+            </div><!--End column-->
+            <div class="clear-block"></div>
+            <p class="view-more"><a href="#">View More &#187;</a></p>
+        </div><!--End inside -->
+    </div><!--End secondary --> 
+    
+    <div id="tertiary">
+        <div class="inside"> 
+            <h3>&#187; Recent News</h3>
+            <h2><a href="#">New Website</a></h2><p>Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est. Sed lectus. Praesent elementum hendrerit tortor. </p>
+            <hr />
+            <h2><a href="#">Other Headlines</a></h2><p>Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est. Sed lectus. Praesent elementum hendrerit tortor.</p> 
+            <p class="view-more"><a href="#">All Posts &#187;</a></p> 
+        </div><!--End inside --> 
+    </div><!--End secondary --> 
+     <div class="clear-block"></div>
+    </div><!--End bottom-container-->
+    
+    <div id="footer">
+        <p>Copyright &copy; Paragon Stables 2009. All Rights Reserved.</p>
+    </div><!--End footer -->
+    
+</div> <!--End container -->
+</body>
+
+</html>
